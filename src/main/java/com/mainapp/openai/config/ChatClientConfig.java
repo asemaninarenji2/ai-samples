061dcf33381d4dbf6eb6c43020ad34e3231f693d
MO2 <asemaninarenji2@gmail.com>
2025-11-17 09:42:49 +1100

    adding new memory chat client

--- src/main/java/com/mainapp/openai/config/ChatClientConfig.java
+++ src/main/java/com/mainapp/openai/config/ChatClientConfig.java
@@ -1,6 +1,7 @@
 package com.mainapp.openai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
@@ -10,25 +11,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatClientConfig {
     //FIRST STYLE OF CREATING A CHAT CLIENT
-    @Bean
-    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+    @Bean("ollamaChatClient")
+    public ChatClient chatClient(OllamaChatModel ollamaChatModel) {
         return ChatClient.create(ollamaChatModel);
     }
 
 
     //SECOND STYLE - MORE CONTROLE TO DICTATE HOW CLIENT SHOULD WORK
-    @Bean
+    @Bean("openAiChatClient")
     public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel){
         ChatClient.Builder builder = ChatClient.builder(openAiChatModel);
         builder.defaultSystem("You are a professional IT desk support");
         return builder.build();
     }
-    @Bean
+    @Bean("openAiChatClientWithOptions")
     public ChatClient openAiChatClientWithOptions(OpenAiChatModel openAiChatModel){
         ChatOptions chatOptions = ChatOptions.builder().model("gpt-4.1-mini").maxTokens(200).temperature(0.8).build();
         ChatClient.Builder builder = ChatClient.builder(openAiChatModel);
         builder.defaultSystem("You are an knowing oracle");
         builder.defaultOptions(chatOptions);
+        return builder.build();
+    }
+    @Bean("chatClientWithMemory")
+    public ChatClient chatClientWithMemory(OpenAiChatModel openAiChatModel){
+        ChatClient.Builder builder = ChatClient.builder(openAiChatModel);
+
         return builder.build();
     }
 
