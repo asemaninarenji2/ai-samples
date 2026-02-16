72d3a17b9f1ca170c29cfd7b7d521d3862346e28
MO2 <asemaninarenji2@gmail.com>
2026-02-16 11:24:04 +1100

    refactoring

--- src/main/java/com/mainapp/openai/config/ChatClientConfig.java
+++ src/main/java/com/mainapp/openai/config/ChatClientConfig.java
@@ -1,17 +1,11 @@
 package com.mainapp.openai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.client.advisor.api.Advisor;
-import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class ChatClientConfig {
@@ -31,18 +25,12 @@ public class ChatClientConfig {
     }
     @Bean("openAiChatClientWithOptions")
     public ChatClient openAiChatClientWithOptions(OpenAiChatModel openAiChatModel){
-        ChatOptions chatOptions = ChatOptions.builder().model("gpt-4.1-mini").maxTokens(200).temperature(0.8).build();
+        ChatOptions chatOptions = ChatOptions.builder().model("gpt-4.1-mini")
+                .maxTokens(200).temperature(0.8).build();
         ChatClient.Builder builder = ChatClient.builder(openAiChatModel);
-        builder.defaultSystem("You are an knowing oracle");
+        builder.defaultSystem("You are an oracle");
         builder.defaultOptions(chatOptions);
         return builder.build();
-    }
-    @Bean("chatClientWithMemory")
-    public ChatClient chatClientWithMemory(OpenAiChatModel chatModel, ChatMemory chatMemory){
-        ChatClient.Builder builder = ChatClient.builder(chatModel);
-        Advisor advisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
-        Advisor advisorLogger = new SimpleLoggerAdvisor();
-        return builder.defaultAdvisors(List.of(advisor, advisorLogger)).build();
     }
 
 
