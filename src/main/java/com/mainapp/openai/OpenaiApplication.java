6cfc7cf07b043ab8277126aa9e571f44dc6c5531
MO2 <asemaninarenji2@gmail.com>
2026-03-04 13:15:18 +1100

    adding web search augmentation

--- src/main/java/com/mainapp/openai/OpenaiApplication.java
+++ src/main/java/com/mainapp/openai/OpenaiApplication.java
@@ -2,8 +2,10 @@ package com.mainapp.openai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OpenaiApplication {
 
 	public static void main(String[] args) {
