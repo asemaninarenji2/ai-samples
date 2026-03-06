67b6f364690ad67547ff596710c67fb2e846f709
MO2 <asemaninarenji2@gmail.com>
2026-03-06 16:04:19 +1100

    psst-retrieve

---
+++ src/main/java/com/mainapp/openai/config/RAGChatClientConfig.java
@@ -0,0 +1,87 @@
+package com.mainapp.openai.config;
+
+import com.mainapp.openai.essentials.customadvisor.TokenUsageAuditAdvisor;
+import com.mainapp.openai.rag.custom.PIIMaskingDocumentPostProcessor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Slf4j
+@Configuration
+public class RAGChatClientConfig {
+
+    //Retrieval Augmentation Advisor beans definition ---------------->
+    @Bean
+    public RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore){
+        return RetrievalAugmentationAdvisor.builder()
+                .documentRetriever(VectorStoreDocumentRetriever.builder().vectorStore(vectorStore)
+                        .topK(3)
+                        .similarityThreshold(0.5)
+                        .build())
+                .build();
+
+    }
+    // (+)                -------------------------------------------
+    @Bean("chatClientWithMemoryAndRetrievalAugmentation")
+    public ChatClient chatClientWithMemoryAndRetrievalAugmentation(OpenAiChatModel chatModel,
+                                                                   @Qualifier("limitedMaxMessages") ChatMemory chatMemory,
+                                                                   RetrievalAugmentationAdvisor retrievalAugmentationAdvisor){
+        ChatClient.Builder builder = ChatClient.builder(chatModel);
+        Advisor advisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        Advisor advisorLogger = new SimpleLoggerAdvisor();
+        Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
+        return builder.defaultAdvisors(List.of(advisor, advisorLogger, tokenUsageAdvisor, retrievalAugmentationAdvisor)).build();
+    }
+    //<-----------------------------------------------------------------
+
+
+    // - ADVANCED RAG
+    // For more effective retrieval tasks, addressing challenges such as poor formed queries,
+    // ambiguous tems, complex vocabulary, unsupported languages etc
+    //Naive RAG:  Retrieve --> Augmentation --> Generation
+    //Advanced RAG:  Pre-retrieval ---> Retrieve ---> Post-retrieval --> Augmentation --> Generation
+
+    @Bean("retrievalAugmentationAdvisorAdvancedRAG")
+    public RetrievalAugmentationAdvisor retrievalAugmentationAdvisorAdvancedRAG(VectorStore vectorStore,
+                                                                                @Qualifier("openAiChatModel") ChatModel chatModel){
+        ChatClient.Builder chatClientBuilder = ChatClient.builder(chatModel);
+        return RetrievalAugmentationAdvisor.builder()
+                .queryTransformers(TranslationQueryTransformer.builder()// This triggers PRE_RETRIEVAL process by calling to LLM
+                        .chatClientBuilder(chatClientBuilder.clone())   // hence you need to give it the chat client builder
+                        .targetLanguage("english")                      //There are multiple QueryTransformer implementations:
+                        .build())                                       //CompressionQueryTransformer, TranslationQueryTransformer, RewriteQueryTransformer
+                                                                        // Refer to documentation of each for use cases.
+                .documentRetriever(VectorStoreDocumentRetriever.builder().vectorStore(vectorStore)// Here search would be based on pre-retrieved documents
+                        .topK(3)
+                        .similarityThreshold(0.5)
+                        .build())
+                .documentPostProcessors(List.of(new PIIMaskingDocumentPostProcessor())) // Implement your own DocumentPostProcessor implementation
+                .build();
+
+    }
+    @Bean("chatClientWithMemoryAndRetrievalAugmentationAndAdvancedRAG")
+    public ChatClient chatClientWithMemoryAndRetrievalAugmentationAndAdvancedRAG(OpenAiChatModel chatModel,
+                                                                   @Qualifier("limitedMaxMessages") ChatMemory chatMemory,
+                                                                   @Qualifier("retrievalAugmentationAdvisorAdvancedRAG") RetrievalAugmentationAdvisor retrievalAugmentationAdvisor){
+        ChatClient.Builder builder = ChatClient.builder(chatModel);
+        Advisor advisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        Advisor advisorLogger = new SimpleLoggerAdvisor();
+        Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
+        return builder.defaultAdvisors(List.of(advisor, advisorLogger, tokenUsageAdvisor, retrievalAugmentationAdvisor)).build();
+    }
+
+}
